b2a5c357c3184b09a41096ae22520c5966acafe3
bigprice <bigangelito@gmail.com>
2023-07-05 23:24:04 -0600

    single container added

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+
+}
